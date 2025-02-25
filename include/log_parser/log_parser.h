1e08882587af3447993e8f9f4a7368092bd647e7
KWoiceshyn <k.woicesh@gmail.com>
2025-02-25 15:22:28 -0700

    Refactor to separate logic

--- include/log_parser/log_parser.h
+++ include/log_parser/log_parser.h
@@ -27,10 +27,13 @@ void getSuccessfulAccessesByURI();
 
 private:
 
-void sortByCountAndPrint(const unordered_map<string, int>& data);
+void processLine(const string& line);
+
+vector<pair<string, int>> sortByCount(const unordered_map<string, int>& data);
 
 string log_file_path_;
-const string code_OK = "200";
+const string code_OK_;
+const string target_request_;
 
 unordered_map<string, int> webserver_accesses_per_host_;
 unordered_map<string, int> successful_accesses_by_uri_;
--- src/log_parser.cpp
+++ src/log_parser.cpp
@@ -1,7 +1,6 @@
 #include "log_parser/log_parser.h"
 
-LogParser::LogParser() {
-    log_file_path_ = "";
+LogParser::LogParser() : log_file_path_(""), code_OK_("200"), target_request_("GET") {
 }
 
 void LogParser::LoadLogFile(const string& log_file_path, const string& start_time, const string& end_time) {
@@ -11,56 +10,64 @@ void LogParser::LoadLogFile(const string& log_file_path, const string& start_tim
     ifstream log_file_stream(log_file_path_);
     string line;
     while (getline(log_file_stream, line)) {
-        istringstream iss(line);
-        string host;
-        string date_time;
-        string request;
-        string uri;
-        string proto;
-        string status;
-        string bytes;
-        iss >> host >> date_time >> request >> uri >> proto >> status >> bytes;
-        if(host.empty()){
-            cerr << "Invalid host name." << endl;
-            continue;
-        }
-        // Remove quotes from request
-        size_t pos = -1;
-        while ((pos = request.rfind('\"')) != string::npos) {
-            request.erase(pos, 1);
-        }
-        //cout << host << " " << date_time << " " << request << " " << uri << " " << proto << " " << status << " " << bytes << endl;
-        if (false) {
-            //TODO check time range
-        } else {
-            webserver_accesses_per_host_[host]++;
-            if (request == "GET" and status == code_OK) {
-                if(uri.empty()){
-                    cerr << "Invalid URI." << endl;
-                    continue;
-                }
-                successful_accesses_by_uri_[uri]++;
+        processLine(line);
+    }
+}
+
+void LogParser::processLine(const string& line) {
+    istringstream iss(line);
+    string host;
+    string date_time;
+    string request;
+    string uri;
+    string proto;
+    string status;
+    string bytes;
+    iss >> host >> date_time >> request >> uri >> proto >> status >> bytes;
+    if(host.empty()){
+        cerr << "Invalid host name." << endl;
+        return;
+    }
+    // Remove quotes from request
+    size_t pos = -1;
+    while ((pos = request.rfind('\"')) != string::npos) {
+        request.erase(pos, 1);
+    }
+    //cout << host << " " << date_time << " " << request << " " << uri << " " << proto << " " << status << " " << bytes << endl;
+    if (false) {
+        //TODO check time range
+    } else {
+        webserver_accesses_per_host_[host]++;
+        if (request == target_request_ and status == code_OK_) {
+            if(uri.empty()){
+                cerr << "Invalid URI." << endl;
+                return;
             }
+            successful_accesses_by_uri_[uri]++;
         }
     }
 }
 
-void LogParser::sortByCountAndPrint(const unordered_map<string, int>& data) {
+vector<pair<string, int>>  LogParser::sortByCount(const unordered_map<string, int>& data) {
     // Copy to vector to sort by counts
     vector<pair<string, int>> data_sorted(data.begin(), data.end());
     sort(data_sorted.begin(), data_sorted.end(), [](const pair<string, int>& a, const pair<string, int>& b) {
         return a.second > b.second;
     });
-    // TODO write output to file
-    for (const auto& [name, count] : data_sorted) {
+    return data_sorted;
+}
+
+void LogParser::getWebserverAccessesByHost() {
+    auto data = sortByCount(webserver_accesses_per_host_);
+    // TODO write output to file ?
+    for (const auto& [name, count] : data) {
         cout << name << " " << count << endl;
     }
 }
 
-void LogParser::getWebserverAccessesByHost() {
-    sortByCountAndPrint(webserver_accesses_per_host_);
-}
-
 void LogParser::getSuccessfulAccessesByURI() {
-    sortByCountAndPrint(successful_accesses_by_uri_);
+    auto data = sortByCount(successful_accesses_by_uri_);
+    for (const auto& [name, count] : data) {
+        cout << name << " " << count << endl;
+    }
 }
